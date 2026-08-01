4261e7b21271319091843502e6396f350a011dac
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-01 22:47:56 +0330

    Add my first Python program

---
+++ basics-01/Mian.py
@@ -0,0 +1,6 @@
+# This is my first Python program 
+# I'm very Happy 
+# this is print method in python language.
+print("Hello world")
+print("My name is Emal Hasanzada ")
+print("This is my first program in python ")
