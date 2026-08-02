49665d60eef8a59ffe2914f9bda0be702a22daa2
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-02 22:02:13 +0330

    this is my first exercise in python

---
+++ basics-01/MyProfile.py
@@ -0,0 +1,27 @@
+# this is my profile class 
+# the variable class is (String, integer, boolean, ane float)
+#       this is my first exercise for python variable
+
+# this is String variable
+name = "Emal"
+print(f"My name is {name}\n")
+
+city = "Tehran"
+print(f"I live is  {city} city")
+
+# this is integer variable
+age = 20
+print(f"I have years {age} old")
+
+# this is float variable
+height = 1.73
+print(f"My Height {height}")
+
+# This is boolean variable 
+# I craet a if loop in this variable.
+is_working = False
+
+if is_working:
+    print("I have a job")
+else:
+    print("I don't hava a job")
