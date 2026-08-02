260af31b9ff01d83b8db4aeebb2d402a0e62be78
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-02 20:14:33 +0330

    This is my first String variable project

---
+++ basics-01/Variable.py
@@ -0,0 +1,12 @@
+# Variable = A container for a value (String , integer, float, boolean)
+#            A variable bebaves as if it was the value it contains
+
+# this is String variable 
+first_name = "Emal"
+food = "pizza"
+email = "emalhasanzada82@gmail.com"
+print(f"You like{food}")
+print(f"Hello{first_name}")
+print(f"Your email is : {email}")
+
+#this is Integer variable.
