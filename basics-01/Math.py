d1e0372bc222bc87c597d0529f7e4aee14d7bf90
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-04 00:58:10 +0330

    This is a class for varaible type and math

---
+++ basics-01/Math.py
@@ -0,0 +1,20 @@
+
+# ingroduction.
+
+print(2 + 3)  # addition(+)
+print(3 - 1)  # subtraction(-)
+print(2 * 3)  # multiplication(*)
+print(3 / 2)  # division(/)
+print(3 ** 2) # exponential(**)
+print(3 % 2)  # modulus(%)
+print(3 // 2) # Floor devision operator(//)
+
+# chack type of variable
+print(type(20))                 # Int
+print(type(3.15))               # float
+print(type(1 + 2j))             # complex
+print(type('Emal'))             # String
+print(type([1, 2, 3]))          # List
+print(type({'name' : 'Emal'}))  # Dictionary
+print(type(9, 8, 2.14, 2.7))    # Tuple
+
