cbaaebcc973e9140707f7137697f6d7029e477e0
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-03 19:29:39 +0330

    This is a class for change variable together

---
+++ basics-01/ChangeVariable.py
@@ -0,0 +1,31 @@
+# this is a class for change (String , float , integer , boolean ) 
+#        varaible together .
+
+name = "Emal"
+age = 20
+gpa = 3.45
+is_student = True
+
+#====================================================
+# this is printing the type of variable for exam
+# there are the type of variable befor the change
+#print(type(name)) # String = 'str'
+#print(type(age)) # integer = 'int'
+#print(type(gpa)) # float = 'float'
+#print(type(is_student)) # boolean = 'bool'
+
+#====================================================
+# change integer to float 
+age = float(age)
+print(age)
+print(type(age)) # this is after change of the variable
+#====================================================
+# change float to integer variable.
+gpa = int(gpa)
+print(gpa)
+print(type(gpa))#this is type of after to change
+#====================================================
+
+
+
+
