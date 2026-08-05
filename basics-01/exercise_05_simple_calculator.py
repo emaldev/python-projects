b48b66d725676f46f77364e021d881691dcbb52f
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-05 19:52:40 +0330

    This is a user information program

--- basics-01/exercise_05_simple_calculator.py
+++ basics-01/exercise_05_simple_calculator.py
@@ -1,6 +1,16 @@
-name = input("Enater your name: ")
-age = int(input("Enter your age :"))
-born = 2026 - age
+# ------------ User information -------------
+ 
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+age = int(input("Enter your age: "))
+country = input("Enter your country:")
+favorite_feild = input("Enter your favorite field:")
 
-print(f"Hello {name} are  {age} years old ")
-print(f"You were born in {born}")
+
+
+print(f"First Name : {first_name}")
+print(f"Last Name  : {last_name}")
+print(f"Age        : {age}")
+print(f"Country    : {country}")
+print(f"Favorite Field : {favorite_feild}")
+
