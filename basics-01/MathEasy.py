5e6ed7e57cfcb15d1a60a69f673a19d7da7b2526
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-05 21:38:23 +0330

    math python

--- basics-01/MathEasy.py
+++ basics-01/MathEasy.py
@@ -1,4 +1,5 @@
-friends = 5
+
+#friends = 5
 
 #friends  += 1
 #friends = friends - 2
@@ -13,9 +14,9 @@ friends = 5
 #friends %= 2
 
 
-x = 3.14
-y = -4
-z = 5
+#x = 3.14
+#y = -4
+#z = 5
 # this is the round funciont .
 #result = round(x)
 
@@ -26,6 +27,18 @@ z = 5
 #result = pow(4,3)
 
 #result = max(x, y, z)
-result = min(x, y, z)
+#result = min(x, y, z)
+
+#print(result)
+import math
+#This is pi 
+math.pi
+print(math.pi)
+
+print(math.e)
+x = 9.9
+#result = math.sqrt(x)
+#result = math.ceil(x)
+
 
 print(result)
