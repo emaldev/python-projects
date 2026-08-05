b753d99e005c5ee73687bdb108447c6856b412ae
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-05 18:54:11 +0330

    this is my first input program with python Happy!

---
+++ basics-01/input.py
@@ -0,0 +1,12 @@
+
+#This is input in python .
+#this is my first program with input by python i am very happy this is my second programing language 
+
+
+name = input("Enter your name :")
+age = int(input("Enter your age: "))
+
+age = age + 1
+
+print(f"My name is {name}")
+print(f"I'm {age} years old")
