239fe05db4b47f886e4430759ea0141910b43704
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-05 20:22:04 +0330

    this is operators in python

---
+++ basics-01/MathEasy.py
@@ -0,0 +1,31 @@
+friends = 5
+
+#friends  += 1
+#friends = friends - 2
+#friends -= 2
+#friends = friends * 3
+#friends *= 3
+#friends = friends / 2
+#friends /= 2
+#friends = friends ** 2
+#friends **= 2
+# remainder = friends % 2
+#friends %= 2
+
+
+x = 3.14
+y = -4
+z = 5
+# this is the round funciont .
+#result = round(x)
+
+#change 
+#result = abs(y)
+
+#this is pow
+#result = pow(4,3)
+
+#result = max(x, y, z)
+result = min(x, y, z)
+
+print(result)
