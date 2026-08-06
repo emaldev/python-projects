d83bc26f17fb21daf3ea7565f57d0ffaf7decfc0
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-07 00:09:39 +0330

    This is a variable practise

--- basics-01/Operator.py
+++ basics-01/Operator.py
@@ -1,8 +1,38 @@
-age = int(input("Enter your age :"))
 
-if age >= 100:
-    print("You are too old to sign up!")
-elif age >= 18:
-    print("You are new siged up!")
-elif age < 0:
-    print("You must be 18 to sign up!")
+first_name = 'Emal '
+last_name = 'Hasanzada'
+country = 'Afghanistan'
+city = 'Kunduz'
+age = '350'
+is_marrid = False
+skills = ['HTML', 'Css', 'Java', 'python', 'c++']
+person_info = {
+    'firstname ': 'Emal', 
+    'lastname' : 'Hasanzada', 
+    'country' : 'Afghanistan', 
+    'city' : 'Kunduz'
+}
+
+#Printing the values stored in the variable 
+print('First name :' , first_name)
+print('First name length: ', len(first_name))
+print('Last name :', last_name)
+print('Lanst name length:', len(last_name))
+print('Country: ', country)
+print('City : ', city)
+print('Age: ', age)
+print('Married: ', is_marrid)
+print('Skills: ', skills)
+print('Person information : ', person_info)
+
+# Declaring multiple variable in one line
+first_name, last_name, country, age, is_marrid = 'Emal', 'Hasanzada', 'Afghanistan', '350', False
+
+print(first_name, last_name, country, age, is_marrid)
+
+print('First name : ', first_name)
+print('Last name :', last_name)
+print('Country : ' , country)
+print('Age: ' , age)
+print('Marride : ', is_marrid)
+
