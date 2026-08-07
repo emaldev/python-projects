c11fabe51bec64f52938176fa723b2a42b05e8ae
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-07 23:07:56 +0330

    thie

---
+++ basics-01/WeatherAdvisor.py
@@ -0,0 +1,13 @@
+
+
+temperature = float(input('Enter teperature to day:'))
+
+if temperature >= 0 and temperature <= 30:
+    print('Perfect weather for a picnic!')
+elif temperature < 10:
+    print('Better stay at home.')
+
+else:
+    print('Hava a nice day!')
+
+print("Don't forget your jacket.")
