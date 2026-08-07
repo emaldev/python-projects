b68f13aa3da36d26fac9228ff817be81e1b90a99
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-07 22:12:43 +0330

    This is a practice project involving operators.

---
+++ basics-01/OperatorPractise.py
@@ -0,0 +1,16 @@
+# logical operators = used on condetional statments
+#               and = checks two or more conditions is True
+#                or = checks if at least one condition is true
+#               not = True if condition is False, and vice versa.
+
+temp = 25
+sunny = True
+if temp > 0 and temp < 30:
+    print('The temperature is good.')
+else:
+    print('The temperature is bad.')
+
+if  not sunny :
+    print('To day is sunny.')
+else:
+    print('Today is cluday.')
