9eb110f46f2429ebcc5cc0a7a76e5cf97a5e58b7
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-08 19:00:14 +0330

    Variable

--- basics-01/WeatherAdvisor.py
+++ basics-01/WeatherAdvisor.py
@@ -1,12 +1,19 @@
-#Password security checher
+#Arthmetic operation in python
+#Integers
 
-password = input('Enter your password: ')
+print('Addition', 1 + 2)
+print('Subtraction:', 2 - 1)
+print('Mulipliction:', 2 * 3)
 
-if len(password) == 0:
-    print('Password cannot be empty!')
-elif '123' in password or password.isdigit():
-    print('Very weak password~')
-elif len(password) >= 8 and " " not in password:
-    print('Strong password!')
-else:
-    print('Weak password!')
+#Division in python gives floating number
+print('Division:', 4 / 2)
+print('Division:', 8 / 2)
+print('Division:', 20 / 5)
+
+# gives without the floating number or the remaining 
+print('Division without the remainder: ', 7 // 2)
+print('Modulus: ', 3 % 2) # Gives the remainder.
+print('Division  without the ramainder: ', 7 // 3)
+print('Exponendtial: ', 3 ** 2) # It's means  3 * 3 
+
+print(3 > 2 and 4 > 3)
